659223f3cfa60a6095ca2108e585b901a9b54f4b
georgsmeinung <jenicolau@gmail.com>
2024-10-14 21:18:08 +0000

    fixex cp_output.sh

--- src/cp_output.sh
+++ src/cp_output.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-cp ./nohup.out ~/buckets/b1/flow/`cat nohup.out | grep "exp_wf_init()" | awk '{print $NF}'`
+OUTPUTDIR=`cat nohup.out | grep "exp_wf_init()" | awk '{print $NF}'`
+TARGET="~/buckets/b1/flow/${OUTPUTDIR}"
+echo "Copying file to $TARGET"
+cp ./nohup.out $TARGET
 
