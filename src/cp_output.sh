c7ae043d4bf39d24e2060fb801ced633fe9f23d6
Jorge Nicolau <22195389+georgsmeinung@users.noreply.github.com>
2024-10-14 18:46:22 -0300

    Fixed target directory string generration

--- src/cp_output.sh
+++ src/cp_output.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 OUTPUTDIR=`cat nohup.out | grep "exp_wf_init()" | awk '{print $NF}'`
-TARGET="~/buckets/b1/flow/${OUTPUTDIR}"
+TARGET="~/buckets/b1/flow/${OUTPUTDIR}/"
 echo "Copying file to $TARGET"
 cp ./nohup.out $TARGET
 
