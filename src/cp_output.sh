103ccb3782292fc3149d7c5716539ace91826e82
georgsmeinung <jenicolau@gmail.com>
2024-10-14 23:29:45 +0000

    auto commit 4       20241014 232939

--- src/cp_output.sh
+++ src/cp_output.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-OUTPUTDIR=`cat nohup.out | grep "exp_wf_init()" | awk '{print $NF}'`
-TARGET="~/buckets/b1/flow/${OUTPUTDIR}/"
+OUTPUTDIR=$(cat nohup.out | grep "exp_wf_init()" | awk '{print $NF}')
+TARGET="$HOME/buckets/b1/flow/${OUTPUTDIR}/"
 echo "Copying file to $TARGET"
-cp ./nohup.out $TARGET
-
+cp ./nohup.out "$TARGET"
