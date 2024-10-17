2780b45185c5b8c565221bbbd24a6e37da348d0b
Jorge Nicolau <22195389+georgsmeinung@users.noreply.github.com>
2024-10-17 10:52:29 -0300

    Fixed semi21.fm ranks = c(3)

--- src/workflows/semi21-929_workflow_SEMI_f202109_viv.r
+++ src/workflows/semi21-929_workflow_SEMI_f202109_viv.r
@@ -462,7 +462,7 @@ wf_SEMI_sep <- function( pnombrewf )
 
   fm <- FM_final_models_lightgbm_semillerio( 
     c(ht, ts9), # los inputs
-    ranks = c(1:3), # 1 = el mejor de la bayesian optimization
+    ranks = c(3), # 1 = el mejor de la bayesian optimization
     semillerio = 20,   # cantidad de semillas finales
     repeticiones_exp = 3 )
   
